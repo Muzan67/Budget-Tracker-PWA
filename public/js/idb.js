f1818a4343ab36192b92554dca19aaf48d5a4a6f
Alejandro Cortez <102841726+Muzan67@users.noreply.github.com>
2022-08-19 13:47:20 -0400

    updated idb file

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,13 +1,13 @@
 // create variable to hold db connection
 let db;
-// establish a connection to IndexDB database called 'pizza_hunt' and set it to version 1
-const request = indexedDB.open("pizza_hunt", 1);
+// establish a connection to IndexDB database called 'budget-tracker' and set it to version 1
+const request = indexedDB.open("budget-tracker", 1);
 // this event will emit if the database version changes (nonexistant to version 1, v1 to v2, etc.)
 request.onupgradeneeded = function (event) {
   // save a reference to the database
   const db = event.target.result;
-  // create an object store (table) called `new_pizza`, set it to have an auto incrementing primary key of sorts
-  db.createObjectStore("new_pizza", { autoIncrement: true });
+  // create an object store (table) called `new_transaction`, set it to have an auto incrementing primary key of sorts
+  db.createObjectStore("new_transaction", { autoIncrement: true });
 };
 
 // upon a successful
@@ -18,7 +18,7 @@ request.onsuccess = function (event) {
   // check if app is online, if yes run uploadPizza() function to send all local db data to api
   if (navigator.onLine) {
     // we haven't created this yet, but we will soon, so let's comment it out for now
-    uploadPizza();
+    uploadTransaction();
   }
 };
 
@@ -30,30 +30,30 @@ request.onerror = function (event) {
 // This function will be executed if we attempt to submit a new pizza and there's no internet connection
 function saveRecord(record) {
   // open a new transaction with the database with read and write permissions
-  const transaction = db.transaction(["new_pizza"], "readwrite");
+  const transaction = db.transaction(["new_transaction"], "readwrite");
 
-  // access the object store for `new_pizza`
-  const pizzaObjectStore = transaction.objectStore("new_pizza");
+  // access the object store for `new_transaction`
+  const budgetObjectStore = transaction.objectStore("new_transaction");
 
   // add record to your store with add method
-  pizzaObjectStore.add(record);
+  budgetObjectStore.add(record);
 }
 
-function uploadPizza() {
+function uploadTransaction() {
   // open a transaction on your db
-  const transaction = db.transaction(["new_pizza"], "readwrite");
+  const transaction = db.transaction(["new_transaction"], "readwrite");
 
   // access your pending object store
-  const pizzaObjectStore = transaction.objectStore("new_pizza");
+  const budgetObjectStore = transaction.objectStore("new_transaction");
 
   // get all records from store and set to a variable
-  const getAll = pizzaObjectStore.getAll();
+  const getAll = budgetObjectStore.getAll();
 
   // upon a successful .getAll() execution, run this function
   getAll.onsuccess = function () {
     // if there was data in indexedDB's store, let's send it to the api server
     if (getAll.result.length > 0) {
-      fetch("/api/pizzas", {
+      fetch("/api/transaction/bulk", {
         method: "POST",
         body: JSON.stringify(getAll.result),
         headers: {
@@ -67,12 +67,12 @@ function uploadPizza() {
             throw new Error(serverResponse);
           }
           // open one more transaction
-          const transaction = db.transaction(["new_pizza"], "readwrite");
-          //access the new_pizza object store
-          const pizzaObjectStore = transaction.objectStore("new_pizza");
+          const transaction = db.transaction(["new_transaction"], "readwrite");
+          //access the new_transaction object store
+          const budgetObjectStore = transaction.objectStore("new_transaction");
           // clear all items in your store
-          pizzaObjectStore.clear();
-          // alert("All saved pizza has been submitted!");
+          budgetObjectStore.clear();
+          alert("All saved transaction has been submitted!");
         })
         .catch((err) => {
           //set reference to redirect back here
@@ -83,4 +83,4 @@ function uploadPizza() {
 }
 
 // listen for app coming back online
-window.addEventListener("online", uploadPizza);
+window.addEventListener("online", uploadTransaction);
